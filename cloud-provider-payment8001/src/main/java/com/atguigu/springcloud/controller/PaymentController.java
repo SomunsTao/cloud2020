44ac897b70a01b891984fee540a7c48f2c97b524
SomunsTao <ctchentao123@qq.com>
2020-05-10 18:41:26 +0800

    。。

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -21,7 +21,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @Value("${server.port}")
-    private String servicePort;
+    private String serverPort;
 
     @Resource
     private DiscoveryClient discoveryClient;
@@ -32,7 +32,7 @@ public class PaymentController {
         int result = paymentService.create(payment);
         log.info("*****插入结果："+result);
         if(result > 0){
-            return new CommonResult(200,"插入数据成功,serverPort"+servicePort,result);
+            return new CommonResult(200,"插入数据成功,serverPort"+serverPort,result);
         }else{
             return new CommonResult(444,"插入数据失败",null);
         }
@@ -42,10 +42,15 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info("*****插入结果："+payment);
         if(payment != null){
-            return new CommonResult(200,"查询成功,serverPort"+servicePort,payment);
+            return new CommonResult(200,"查询成功,serverPort"+serverPort,payment);
         }else{
             return new CommonResult(444,"没有对应记录,查询ID："+id,null);
         }
+    }
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB()
+    {
+        return serverPort;
     }
 
     @GetMapping(value = "/payment/discovery")
